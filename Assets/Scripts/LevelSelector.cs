c0aaa6778aff74c802b41ee1c887931dead0a653
André Santos <santos.as96@hotmail.com>
2018-07-25 04:33:52 +0100

    *Started the implementation of a level selector; *Added animated transitions between main menu and level selector;

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject Worlds;
+    
+    public void NextWorld()
+    {
+        Worlds.GetComponent<RectTransform>().anchoredPosition = new Vector2(Worlds.GetComponent<RectTransform>().anchoredPosition.x-550f, Worlds.GetComponent<RectTransform>().anchoredPosition.y);
+    }
+    public void PreviewWorld()
+    {
+        Worlds.GetComponent<RectTransform>().anchoredPosition = new Vector2(Worlds.GetComponent<RectTransform>().anchoredPosition.x + 550f, Worlds.GetComponent<RectTransform>().anchoredPosition.y);
+    }
+}
