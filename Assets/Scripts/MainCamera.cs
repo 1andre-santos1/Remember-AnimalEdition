8f5bb7e2f0a420bd13a17a8eb12cb9f8ba368867
André Santos <santos.as96@hotmail.com>
2018-07-30 13:00:52 +0100

    *Added moving animation to the main menu camera;

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public float minX = -7f;
+    public float maxX = 25f;
+    public float speed = 5f;
+
+    private void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0f,0f);
+        if (transform.position.x > maxX || transform.position.x < minX)
+            speed = -speed;
+    }
+
+}
